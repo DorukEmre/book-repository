eb5609e6d02412da9cd879fd2e8238fb37de1cd9
DorukEmre <doruk.emre@outlook.com>
2022-06-18 10:32:51 +0100

    initial commit - can display books and add new

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const books = [
+  {
+    title: "Hi there!",
+    author: "Amando",
+    added: new Date()
+  },
+  {
+    title: "Hello World!",
+    author: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { books: books });
+});
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+router.post('/new', function(req, res, next) {
+  const author = req.body.author;
+  const title = req.body.title;
+  console.log(author, title)
+  books.push({
+      title: title, 
+      author: author, 
+      added: new Date()
+    });
+  res.redirect("/");
+});
+
+module.exports = router;
